5cfa94f60220e59363260eb8c3a07d4827224156
CoderSid007 <siddhesdas2020@gmail.com>
2023-08-07 21:24:31 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+//import 'package:quiz_app/start_screen.dart';
+//import 'questions_screen.dart';
+
+void main() 
+{
+  // ignore: prefer_const_constructors
+  runApp(Center(
+    
+    child: const Quiz()
+    ) 
+  );
+}
