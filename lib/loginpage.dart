b86051a0ec7bbe2f92949a9879b3ad786f5c29ad
CoderSid007 <siddhesdas2020@gmail.com>
2023-08-14 00:58:48 +0530

    14-08-23

---
+++ lib/loginpage.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your email address';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+               Quiz;
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/loginpage.dart';
+
 //import 'package:quiz_app/start_screen.dart';
 //import 'questions_screen.dart';
 
 void main() 
 {
   // ignore: prefer_const_constructors
-  runApp(Center
-  (child: const Quiz()) 
+  
+  runApp(const Center
+  (child: LoginPage()) 
   );
 }
 
